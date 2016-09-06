0fb065651286458dd4db4b327edec9a0952c3920
andrew condon <andrew.condon@gmail.com>
2016-09-06 10:38:45 +0200

    simplest example working with d3v4 now

--- src/Main.purs
+++ src/Main.purs
@@ -1,44 +1,31 @@
 module Main where
 
 import Control.Monad.Eff (Eff)
-import Prelude (show, id, bind, (<>), (*))
+import Prelude (show, id, bind, (<>), (*), max)
+import Data.Foldable (foldr)
 import Graphics.D3.Base (D3)
 import Graphics.D3.Util (maxFn, (..))
 import Graphics.D3.Selection (Selection, text', style', append, enter, bindData, selectAll, rootSelect)
 import Graphics.D3.Scale (toFunction, range, domain, linearScale)
 
--- | This is a PureScript adaptation of part 1 of Mike Bostock's "Let's Make a Bar Chart" series:
--- | http://bost.ocks.org/mike/bar/1/
-
-{-
-Original JavaScript code:
-=========================
-var data = [4, 8, 15, 16, 23, 42];
-var x = d3.scale.linear()
-    .domain([0, d3.max(data)])
-    .range([0, 420]);
-d3.select(".chart")
-  .selectAll("div")
-    .data(data)
-  .enter().append("div")
-    .style("width", function(d) { return x(d) + "px"; })
-    .text(function(d) { return d; });
--}
-
 array :: Array Number
 array = [4.0, 8.0, 15.0, 16.0, 23.0, 42.0]
+
+arrayMax = foldr max 0.0 array
+
+-- x d = d * 10.0
 
 -- main :: forall e. Eff (d3 :: D3, dom :: DOM, console :: CONSOLE | e) Unit
 main :: forall e. Eff ( d3 :: D3 | e ) (Selection Number)
 main = do
-  -- x <- linearScale
-  --   .. domain [0.0, maxFn id array]
-  --   .. range [0.0, 420.0]
-  --   .. toFunction
-  --
+  x <- linearScale
+    .. domain [0.0, arrayMax]
+    .. range [0.0, 420.0]
+    .. toFunction
+
   rootSelect ".chart"
     .. selectAll "div"
       .. bindData array
     .. enter .. append "div"
-      .. style' "width" (\d -> show (d * 10.0) <> "px") -- (\d -> show (x d) <> "px")
+      .. style' "width" (\d -> show (x d) <> "px") -- (\d -> show (x d) <> "px")
       .. text' show
