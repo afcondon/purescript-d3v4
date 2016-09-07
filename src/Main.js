6c6a3aedd77955e4c42125cd34ea8ce36c7ab138
andrew condon <andrew.condon@gmail.com>
2016-09-07 10:56:30 +0200

    first working example

--- src/Main.js
+++ src/Main.js
@@ -1,37 +1,18 @@
 /* global exports */
 "use strict";
-exports.rootSelectImpl    = rootSelect
-exports.selectAllImpl     = selectAll
-exports.bindDataImpl      = bindData
-exports.enterImpl         = enter
-exports.unsafeStyleImplP  = unsafeStyleP
-exports.unsafeTextImplP   = unsafeTextP
-exports.unsafeInsertImpl  = unsafeInsert
-exports.unsafeAppendImpl  = unsafeAppend
-
-// functions for typeclass "Appendable"
-function unsafeInsert(selector, selection) {
-  return selection.insert(selector);
-}
-function unsafeAppend(tag, selection) {
-  return selection.append(tag);
-}
+exports.enterImpl         = function (update)               { return update.enter(); }
+exports.rootSelectImpl    = function (selector)             { return d3.select(selector); }
+exports.selectAllImpl     = function (selector, selection)  { return selection.selectAll(selector); }
+exports.bindDataImpl      = function (array, selection)     { return selection.data(array); }
+exports.unsafeStyleImpl   = function (key, val, selection)  { return selection.style(key, val); }
+exports.unsafeStyleImplP  = function (key, val, selection)  { return selection.style(key, val); }
+exports.unsafeStyleImplPP = function (key, val, selection)  { return selection.style(key, function (d, i) { return val(d)(i); }) }
+exports.unsafeTextImpl    = function (text, selection)      { return selection.text(text); }
+exports.unsafeTextImplP   = function (text, selection)      { return selection.text(text); }
+exports.unsafeTextImplPP  = function (text, selection)      { return selection.text(function (d,i) { return text(d)(i); }); }
+exports.unsafeInsertImpl  = function (selector, selection)  { return selection.insert(selector); }
+exports.unsafeAppendImpl  = function (tag, selection)       { return selection.append(tag); }
 
 function rootSelect(selector) {
   return d3.select(selector);
-}
-function selectAll(selector, selection) {
-  return selection.selectAll(selector);
-}
-function bindData(array, selection) {
-  return selection.data(array);
-}
-function enter(update) {
-  return update.enter();
-}
-function unsafeStyleP(key, val, selection) {
-  return selection.style(key, val);
-}
-function unsafeTextP(text, selection) {
-  return selection.text(text);
 }
