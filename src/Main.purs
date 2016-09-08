abd2d553261f3bd7c3df6d62b0021eeed339a838
andrew condon <andrew.condon@gmail.com>
2016-09-08 11:38:47 +0200

    more parts of Selection
    demonstrated working with predicate functions for classes and attributes - still needs to properly handle ‘this’ in those, i think

--- src/Main.purs
+++ src/Main.purs
@@ -4,9 +4,10 @@ import D3.Selection
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 import DOM.HTML.Event.EventTypes (mouseenter, mouseleave, click)
+import Data.Array (reverse)
 import Data.Foldable (foldr)
-import Data.String (toUpper, length)
-import Prelude (Unit, show, unit, pure, bind, max, (*), (<>))
+import Data.String (length, toCharArray, fromCharArray)
+import Prelude (Unit, show, unit, pure, bind, max, (*), (<>), (<<<), (==))
 
 {-
 -- next target is to handle this case:
@@ -34,6 +35,9 @@ array2 = ["this", "is", "a", "sentence", "in", "pieces"]
 arrayMax :: Number
 arrayMax = foldr max 0.0 array
 
+revString :: String -> String
+revString = fromCharArray <<< reverse <<< toCharArray
+
 awn :: forall eff. CallbackParam Number -> Eff (d3::D3, console::CONSOLE|eff) Unit
 awn { datum: d, meta: m } = do
   log (show d)
@@ -46,6 +50,12 @@ bel { datum: d, prop: p } = do
   log (show p)
   pure unit
 
+cep :: Number -> Index -> Nodes -> D3Element -> Boolean
+cep datum _ _ _ = if (datum == 16.0) then true else false
+
+dof :: String -> Index -> Nodes -> D3Element -> String
+dof datum _ _ _ = if (datum == "sentence") then "marriage" else theHorror
+
 main :: forall e. Eff ( d3 :: D3 , console :: CONSOLE | e ) Unit
 main = do
   d3Select ".chart"
@@ -53,16 +63,21 @@ main = do
       .. dataBind array
     .. enter .. append "div"
       .. style "width" (FnD (\d -> show (d * 10.0) <> "px"))
-      .. text          (FnD (\d -> show d))
+      .. classed "twice as nice" (ClassFn (\d i nodes el -> i == 2.0 ))
+      .. classed "sweet sixteen" (ClassFn cep)
+      .. attr "name"             (AttrV "fred")
+      .. text                    (FnD (\d -> show d))
       .. on mouseenter         awn
       .. on mouseleave         awn
       .. on' click "magic" "snape" bel
   d3Select ".chart2"
     .. selectAll "div"
-      .. dataBindKeyFn array2 (\d -> toUpper d)
+      .. dataBindKeyFn array2 (\d -> revString d)
     .. enter .. append "div"
       .. style "background-color"  (Value "red")
-      .. style "width" (FnD (\d -> show ((length d) * 20) <> "px"))
-      .. text          (FnD (\d -> show d))
+      .. style "width"             (FnD (\d -> show ((length d) * 20) <> "px"))
+      .. classed "foo bar"         (ClassB true)
+      .. attr "name"               (AttrFn dof)
+      .. text                      (FnD (\d -> show d))
       .. on' click "cep" "stringy" bel
   pure unit
