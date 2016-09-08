3b2ddb9ba07b021a929e2db0e5c51cd83d058fe5
andrew condon <andrew.condon@gmail.com>
2016-09-08 20:04:09 +0200

    a slightly evil commit
    thru the magic of just pushing the problem into JavaScript and having it come back out typed we resolve the afore-mentioned problem of saved transitions having their types fixed.
    Basically,  casting. But then it is an accurate model of what D3 is doing and the desire is not to make a strongly typed D3 but to enable D3 modules and idioms to be available in Purescript programs

--- src/Transition.purs
+++ src/Transition.purs
@@ -38,7 +38,9 @@ foreign import data Transition :: * -> *
 foreign import d3TransitionFn    :: forall d eff.   EffFn1 (d3::D3|eff) String                           (Transition d)
 foreign import transitionFn      :: forall d eff.   EffFn1 (d3::D3|eff)                   (Selection d)  (Transition d)
 foreign import namedTransitionFn :: forall d eff.   EffFn2 (d3::D3|eff) String            (Selection d)  (Transition d)
-foreign import savedTransitionFn :: forall d eff.   EffFn2 (d3::D3|eff) (Transition d)    (Selection d)  (Transition d)
+-- when using a saved transition you're going to be using it with different types of selections to this function will
+-- morph it from type x to type d, the type of the selection to which it is being applied
+foreign import savedTransitionFn :: forall d x eff.   EffFn2 (d3::D3|eff) (Transition x)  (Selection d)  (Transition d)
 foreign import attrFn            :: forall d v eff. EffFn3 (d3::D3|eff) String v          (Transition d) (Transition d)
 foreign import attrIFn :: forall d v eff. EffFn3 (d3::D3|eff) String (InterpolatorFn v d) (Transition d) (Transition d)
 foreign import durationFn        :: forall d eff. EffFn2 (d3::D3|eff) Number              (Transition d) (Transition d)
@@ -55,7 +57,7 @@ makeTransition        = runEffFn1 transitionFn
 namedTransition :: forall d eff. String               -> Selection d  -> Eff (d3::D3|eff) (Transition d)
 namedTransition name  = runEffFn2 namedTransitionFn name
 
-savedTransition :: forall d eff. (Transition d)       -> Selection d  -> Eff (d3::D3|eff) (Transition d)
+savedTransition :: forall d x eff. (Transition x)       -> Selection d  -> Eff (d3::D3|eff) (Transition d)
 savedTransition name  = runEffFn2 savedTransitionFn name
 
 duration :: forall d eff. Number                      -> Transition d -> Eff (d3::D3|eff) (Transition d)
