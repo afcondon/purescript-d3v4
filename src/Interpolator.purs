cd08199126cba455ec70dba0f7bdb00b99a8387a
andrew condon <andrew.condon@gmail.com>
2016-09-12 19:01:13 +0200

    tidy up and some renaming

--- src/Interpolator.purs
+++ src/Interpolator.purs
@@ -18,12 +18,12 @@ type Index = Number
 -- foreign import data TweenFn :: * -> *
 foreign import data D3EffTweenTarget ::     # ! -> * -> * -> * -> *                          -> *
 type D3TweenTargetUncurried v d = ∀ eff.
-                  D3EffTweenTarget eff             d    Index     D3Element                     v
+                  D3EffTweenTarget (d3::D3|eff)    d    Index     D3Element                     v
 type D3TweenTarget          v d = ∀ eff.
                                                   (d -> Index ->  D3Element -> Eff (d3::D3|eff) v)
 
 foreign import data D3EffTweenFn ::     # ! -> * -> * -> * -> *                           -> *
 type D3TweenFnUncurried v d = ∀ eff.
-                  D3EffTweenFn eff             d    Index     D3Element                     (Time -> v)
+                  D3EffTweenFn (d3::D3|eff)    d    Index     D3Element                     (Time -> v)
 type D3TweenFn          v d = ∀ eff.
                                               (d -> Index -> D3Element -> Eff (d3::D3|eff) (Time -> v))
