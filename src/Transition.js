c39a08f1b01c37754b994bc43db954f6e57344f7
andrew condon <andrew.condon@gmail.com>
2016-09-13 11:47:26 +0200

    adds a couple more functions on selections
    and transitions
    
    in particular delay - which also shows that changes are needed to wrap / uncurry the DIE callbacks for other functions

--- src/Transition.js
+++ src/Transition.js
@@ -1,17 +1,21 @@
 /* global exports */
 "use strict";
 
-exports.d3TransitionFn    = function (name)              { return d3.transition(name); }
+exports.d3TransitionFn    = function (name)                     { return d3.transition(name); }
 
-exports.transitionFn      = function (selection)       { return selection.transition(); }
-exports.namedTransitionFn = function (name, selection) { return selection.transition(name); }
-exports.savedTransitionFn = function (t, selection)    { return selection.transition(t); }
-exports.durationFn        = function (ms, transition)  { return transition.duration(ms); }
 exports.attrFn            = function (name, value, transition)  { return transition.attr(name, value); }
 exports.attrIFn           = function (name, interp, transition) { return transition.attr(name, interp); }
+exports.durationFn        = function (ms, transition)           { return transition.duration(ms); }
+exports.emptyFn           = function (transition)               { return transition.empty(); }
 exports.mergeFn           = function (other, transition)        { return transition.merge(other); }
+exports.namedTransitionFn = function (name, selection)          { return selection.transition(name); }
 exports.nodeFn            = function (transition)               { return transition.node(); }
 exports.nodesFn           = function (transition)               { return transition.nodes(); }
+exports.savedTransitionFn = function (t, selection)             { return selection.transition(t); }
 exports.styleFn           = function (name, value, transition)  { return transition.style(name, value); }
 exports.styleIFn          = function (name, interp, transition) { return transition.style(name, interp); }
 exports.styleTweenFn      = function (name, interp, transition) { return transition.styleTween(name, interp); }
+exports.transitionFn      = function (selection)                { return selection.transition(); }
+exports.transition2Fn     = function (transition)               { return transition.transition(); }
+exports.delayFn           = function (ms, transition)           { return transition.delay(ms); }
+exports.delayIFn          = function (fn, transition)           { return transition.delay(fn); }
